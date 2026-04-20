3e587f0da3883f55397f0591cc34fab579158cd0
madhumithaandavar04 <madhumitha.a@cdw.com>
2026-04-21 00:10:03 +0530

    Update fileService.js

--- task_4/services/fileService.js
+++ task_4/services/fileService.js
@@ -1,10 +1,10 @@
 import fs from "fs"
-//load dotenv
-const db="/Users/madhumithaa/Desktop/node_projects/task_4/data/"+process.env.DB;
+// load dotenv
+const db = "/Users/madhumithaa/Desktop/node_projects/task_4/data/"+process.env.DB;
 /**
  * create the database file if doesn't exist 
  */
-export const initializeFile=()=>{
+export const initializeFile = () => {
     if(fs.existsSync(db)){
         return;
     }
@@ -18,7 +18,7 @@ throw error;
  * read the database file and return the data
  * @returns data in the file
  */
-export const readFile=()=>{
+export const readFile = () => {
     try{
 const data=fs.readFileSync(db,'utf8');
 return JSON.parse(data);
@@ -31,7 +31,7 @@ throw error;
  * get the data from the user and write it into the db file
  * @param {*} data 
  */
-export const writeFile=(data)=>{
+export const writeFile = (data) => {
     try{
         fs.writeFileSync(db,JSON.stringify(data));
     }catch(error){
