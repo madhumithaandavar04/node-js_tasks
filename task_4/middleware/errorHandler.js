caba5ee5090547f7cba2c9a49a53e7e1bf8c776f
madhumithaandavar04 <madhumitha.a@cdw.com>
2026-04-20 22:29:39 +0530

    Delete unwanted files

--- task_4/middleware/errorHandler.js
+++
@@ -1,5 +0,0 @@
-//error handler middleware to handle error
-export const errorhandler=(error,req,res,next)=>{
-    const statusCode = error.status||500;
-return res.status(statusCode).send(error.message);
-}
