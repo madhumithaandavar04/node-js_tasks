f255b970c858b49add58dbed5c4272943a336f53
madhumithaandavar04 <madhumitha.a@cdw.com>
2026-04-21 07:52:45 +0530

    added usermodels

--- task_5/middleware/errorHandler.js
+++ task_5/middleware/errorHandler.js
@@ -1,9 +1,9 @@
 import { logger } from "../logger/config.js";
 //error handler middleware
-export const errorhandler=(error,req,res,next)=>{
-    logger.error(`${error.status||500}-${error.message}`);
-    res.status(error.status||500).json({
-        success:false,
-        message:error.message||"Internal Server error"
+export const errorhandler = (error, req, res, next) => {
+    logger.error(`${error.status || 500}-${error.message}`);
+    res.status(error.status || 500).json({
+        success: false,
+        message: error.message || "Internal Server error"
     });
 }
--- task_5/models/userModel.js
+++ task_5/models/userModel.js
@@ -1,24 +1,27 @@
 import mongoose from "mongoose"
 //user schema
-const userSchema=new mongoose.Schema({
-employeeId:{
-  type:Number,
-  required:true
-},
-realName:{
-  type:String,
-  required:true},
-nickName:{
-  type:String,
-  required:true},
-dob:{
-  type:Date,
-  required:true},
-hobbies:{
-  type:[{type:String}],
-  required:true
-}
+const userSchema = new mongoose.Schema({
+  employeeId: {
+    type: Number,
+    required: true
+  },
+  realName: {
+    type: String,
+    required: true
+  },
+  nickName: {
+    type: String,
+    required: true
+  },
+  dob: {
+    type: Date,
+    required: true
+  },
+  hobbies: {
+    type: [{ type: String }],
+    required: true
+  }
 })
 //create user model
-const User=mongoose.model('User',userSchema);
+const User = mongoose.model('User', userSchema);
 export default User;
