4580439ab628564d99e34881b8949e539f296bde
madhumithaandavar04 <madhumitha.a@cdw.com>
2026-05-11 12:57:07 +0530

    optimized the random color generator

--- task_1/index.js
+++ task_1/index.js
@@ -1,9 +1,20 @@
 const fs = require('fs')
 const data = fs.readFileSync('./data/color_ palette.json', 'utf8');
 const colorPalette = JSON.parse(data);
-const colors = colorPalette.map((colorObject) => colorObject["color"]);
-//random color generator
-const randomColors = colors.sort((a, b) => Math.random() - 0.5).slice(0, 5);
+const count=5;
+const length=colorPalette.length;
+const limit=Math.min(length,count)
+const randomColors=[]
+const selectedIndices=new Set();
+while (selectedIndices.size < limit) {
+   let randomIndex=Math.floor(Math.random()*length)
+   //if the random index not in set we can add the random index to the set
+   if (!selectedIndices.has(randomIndex)) {
+      selectedIndices.add(randomIndex);
+      //add the color to the random array
+      randomColors.push(colorPalette[randomIndex].color); 
+    }
+}
 console.log(randomColors);
 //write file random color palette
 const colorsJson = JSON.stringify(randomColors);
